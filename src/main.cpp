9da268ac690571bd1ec26c52d3651b7f5123aa6b
senwa105 <senwa105@gmail.com>
2023-01-02 11:53:36 -0500

    added fps to example

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,9 @@
 int main(void) {
     const int screenWidth = 800;
     const int screenHeight = 450;
+    const int fps = 60;
 
-    Game game{screenWidth, screenHeight, "Tetris Example"};
+    Game game{screenWidth, screenHeight, fps, "Tetris Example"};
 
     while (!game.ShouldClose()) {
         game.Tick();
