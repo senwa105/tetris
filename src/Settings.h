50192ec40d239b3d6b17f889ce8c29715516ea1d
senwa105 <senwa105@gmail.com>
2023-01-02 17:19:31 -0500

    made constexprs inline for external linkage

--- src/Settings.h
+++ src/Settings.h
@@ -5,16 +5,15 @@
 
 namespace settings {
     // Window settings
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 600;
-    constexpr int fps = 60;
+    inline constexpr int screenWidth = 800;
+    inline constexpr int screenHeight = 600;
+    inline constexpr int fps = 60;
 
     // Board settings
-    constexpr Vec2<int> board_position{200, 200};
-    constexpr Vec2<int> board_width_height{10, 20};
-    constexpr int cell_size = 20;
-    constexpr int cell_padding = 2;
+    inline constexpr Vec2<int> board_position{200, 100};
+    inline constexpr Vec2<int> board_width_height{10, 20};
+    inline constexpr int cell_size = 20;
+    inline constexpr int cell_padding = 2;
 }
-
 
 #endif
