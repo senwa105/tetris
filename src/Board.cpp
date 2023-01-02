dc29cb57d1a8b115428db3c6100e977dc75cc856
senwa105 <senwa105@gmail.com>
2023-01-02 12:31:58 -0500

    implemented Board class

---
+++ src/Board.cpp
@@ -0,0 +1,53 @@
+#include "Board.h"
+
+#include <cassert>
+
+Board::Cell::Cell()
+    : exists_(false),
+      color_(WHITE)
+{}
+
+void Board::Cell::SetColor(Color c) {
+    color_ = c;
+    exists_ = true;
+}
+
+Color Board::Cell::GetColor() const {
+    return color_;
+}
+
+void Board::Cell::Remove() {
+    exists_ = false;
+}
+
+Board::Board(int x, int y, int width, int height, int cell_size)
+    : screen_x_(x),
+      screen_y_(y),
+      width_(width),
+      height_(height),
+      cell_size_(cell_size)
+{
+    assert(width_ > 0 && height_ > 0);
+    assert(cell_size_ > 0);
+    cells_.resize(width_ * height_);
+}
+
+void Board::SetCell(int x, int y, Color c) {
+    assert(x >= 0 && x < width_ && y >= 0 && y < height_ && "Cell coordinates must be inbounds");
+    cells_[y * width_ + x].SetColor(c);
+}
+
+void Board::DrawCell(int x, int y) const {
+    assert(x >= 0 && x < width_ && y >= 0 && y < height_ && "Cell coordinates must be inbounds");
+    DrawRectangle(screen_x_ + x * cell_size_,
+                  screen_y_ + y * cell_size_,
+                  cell_size_,
+                  cell_size_,
+                  cells_[y * width_ + x].GetColor());
+}
+
+void Board::Draw() const {
+    for (int y = 0; y < height_; y++)
+        for (int x = 0; x < width_; x++)
+            DrawCell(x, y);
+}
---
+++ src/Board.h
@@ -0,0 +1,36 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <vector>
+
+#include "raylib.h"
+
+class Board {
+private:
+    class Cell {
+    private:
+        bool exists_;
+        Color color_;
+    
+    public:
+        Cell();
+        void SetColor(Color c);
+        Color GetColor() const;
+        void Remove();
+    };
+
+    std::vector<Cell> cells_;
+    const int screen_x_;
+    const int screen_y_;
+    const int width_;
+    const int height_;
+    const int cell_size_;
+
+public:
+    Board(int x, int y, int width, int height, int cell_size);
+    void SetCell(int x, int y, Color c);
+    void DrawCell(int x, int y) const;
+    void Draw() const;
+};
+
+#endif
