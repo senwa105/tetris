a7c5b201c00ae4624de608e5b7c942d50265c601
senwa105 <senwa105@gmail.com>
2023-01-03 17:15:30 -0500

    added ctors for individual minos

--- src/Tetromino.h
+++ src/Tetromino.h
@@ -1,6 +1,8 @@
 #ifndef TETROMINO_H
 #define TETROMINO_H
 
+#include <cassert>
+
 #include "raylibcpp.h"
 #include "Vec2.hpp"
 #include "Board.h"
@@ -10,73 +12,124 @@ private:
     const bool* shape_;
     const int dimension_;
     const Color color_;
+    const Board& board_;
     Vec2<int> board_position_;
 
 public:
     Tetromino(const bool* shape, int dimension, Color color, const Board& board);
+    void Draw() const;
 };
 
 class IMino : public Tetromino {
 private:
-    static constexpr bool shape[] {0, 0, 0, 0,
-                                   1, 1, 1, 1,
-                                   0, 0, 0, 0,
-                                   0, 0, 0, 0};
-    static constexpr int dimension = 4;
-    static constexpr Color color = SKYBLUE;
+    static constexpr bool shape_[] {0, 0, 0, 0,
+                                    1, 1, 1, 1,
+                                    0, 0, 0, 0,
+                                    0, 0, 0, 0};
+    static constexpr int dimension_ = 4;
+    static constexpr Color color_ = SKYBLUE;
+
+public:
+    IMino(Board& board)
+        : Tetromino(shape_, dimension_, color_, board)
+    {
+        static_assert(sizeof(shape_) / sizeof(bool) == dimension_ * dimension_);
+    }
 };
 
 class OMino : public Tetromino {
 private:
-    static constexpr bool shape[] {1, 1,
-                                   1, 1};
-    static constexpr int dimension = 2;
-    static constexpr Color color = YELLOW;
+    static constexpr bool shape_[] {1, 1,
+                                    1, 1};
+    static constexpr int dimension_ = 2;
+    static constexpr Color color_ = YELLOW;
+
+public:
+    OMino(Board& board)
+        : Tetromino(shape_, dimension_, color_, board)
+    {
+        static_assert(sizeof(shape_) / sizeof(bool) == dimension_ * dimension_);
+    }
 };
 
 class TMino : public Tetromino {
 private:
-    static constexpr bool shape[] {0, 1, 0,
-                                   1, 1, 1,
-                                   0, 0, 0};
-    static constexpr int dimension = 3;
-    static constexpr Color color = MAGENTA;
+    static constexpr bool shape_[] {0, 1, 0,
+                                    1, 1, 1,
+                                    0, 0, 0};
+    static constexpr int dimension_ = 3;
+    static constexpr Color color_ = MAGENTA;
+
+public:
+    TMino(Board& board)
+        : Tetromino(shape_, dimension_, color_, board)
+    {
+        static_assert(sizeof(shape_) / sizeof(bool) == dimension_ * dimension_);
+    }
 };
 
 class JMino : public Tetromino {
 private:
-    static constexpr bool shape[] {1, 0, 0,
-                                   1, 1, 1,
-                                   0, 0, 0};
-    static constexpr int dimension = 3;
-    static constexpr Color color = DARKBLUE;
+    static constexpr bool shape_[] {1, 0, 0,
+                                    1, 1, 1,
+                                    0, 0, 0};
+    static constexpr int dimension_ = 3;
+    static constexpr Color color_ = DARKBLUE;
+
+public:
+    JMino(Board& board)
+        : Tetromino(shape_, dimension_, color_, board)
+    {
+        static_assert(sizeof(shape_) / sizeof(bool) == dimension_ * dimension_);
+    }
 };
 
 class LMino : public Tetromino {
 private:
-    static constexpr bool shape[] {0, 0, 1,
-                                   1, 1, 1,
-                                   0, 0, 0};
-    static constexpr int dimension = 3;
-    static constexpr Color color = ORANGE;
+    static constexpr bool shape_[] {0, 0, 1,
+                                    1, 1, 1,
+                                    0, 0, 0};
+    static constexpr int dimension_ = 3;
+    static constexpr Color color_ = ORANGE;
+
+public:
+    LMino(Board& board)
+        : Tetromino(shape_, dimension_, color_, board)
+    {
+        static_assert(sizeof(shape_) / sizeof(bool) == dimension_ * dimension_);
+    }
 };
 
 class SMino : public Tetromino {
 private:
-    static constexpr bool shape[] {0, 1, 1,
-                                   1, 1, 0,
-                                   0, 0, 0};
-    static constexpr int dimension = 3;
-    static constexpr Color color = GREEN;
+    static constexpr bool shape_[] {0, 1, 1,
+                                    1, 1, 0,
+                                    0, 0, 0};
+    static constexpr int dimension_ = 3;
+    static constexpr Color color_ = GREEN;
+
+public:
+    SMino(Board& board)
+        : Tetromino(shape_, dimension_, color_, board)
+    {
+        static_assert(sizeof(shape_) / sizeof(bool) == dimension_ * dimension_);
+    }
 };
 
 class ZMino : public Tetromino {
 private:
-    static constexpr bool shape[] {1, 1, 0,
-                                   0, 1, 1,
-                                   0, 0, 0};
-    static constexpr int dimension = 3;
-    static constexpr Color color = RED;
+    static constexpr bool shape_[] {1, 1, 0,
+                                    0, 1, 1,
+                                    0, 0, 0};
+    static constexpr int dimension_ = 3;
+    static constexpr Color color_ = RED;
+
+public:
+    ZMino(Board& board)
+        : Tetromino(shape_, dimension_, color_, board)
+    {
+        static_assert(sizeof(shape_) / sizeof(bool) == dimension_ * dimension_);
+    }
 };
 
 #endif
