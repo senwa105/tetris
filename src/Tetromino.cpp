34a34ebe3dc9272d829f60a396fecb455e9af1d2
senwa105 <senwa105@gmail.com>
2023-01-04 03:35:19 -0500

    all rotations are now statically defined

--- src/Tetromino.cpp
+++ src/Tetromino.cpp
@@ -1,7 +1,8 @@
 #include "Tetromino.h"
+#include <iostream>
 
-Tetromino::Tetromino(const bool* shape, int dimension, Color color, const Board& board)
-    : shape_(shape),
+Tetromino::Tetromino(const bool* shapes, int dimension, Color color, const Board& board)
+    : shapes_(shapes),
       dimension_(dimension),
       color_(color),
       board_(board),
@@ -9,27 +10,35 @@ Tetromino::Tetromino(const bool* shape, int dimension, Color color, const Board&
       current_rotation_(Rotation::R0)
 {}
 
+bool Tetromino::IsCollision() {
+
+}
+
 void Tetromino::Draw() const {
     for (int y = 0; y < dimension_; y++)
         for (int x = 0; x < dimension_; x++) {
-            bool cell = false;
-            switch (current_rotation_) {
-            case Rotation::R0:
-                cell = shape_[y*dimension_ + x];
-                break;
-            case Rotation::R90:
-                cell = shape_[dimension_*(dimension_ - 1) - x*dimension_ + y];
-                break;
-            case Rotation::R180:
-                cell = shape_[dimension_*dimension_ - 1 - y*dimension_  - x];
-                break;
-            case Rotation::R270:
-                cell = shape_[dimension_ - 1 + x*dimension_ - y];
-                break;
-            default:
-                break;
-            }
+            // bool cell = false;
+            // switch (current_rotation_) {
+            // case Rotation::R0:
+            //     cell = shapes_[rotation_chunk + y*dimension_ + x];
+            //     break;
+            // case Rotation::R90:
+            //     cell = shapes_[rotation_chunk + dimension_*(dimension_ - 1) - x*dimension_ + y];
+            //     break;
+            // case Rotation::R180:
+            //     cell = shapes_[rotation_chunk + dimension_*dimension_ - 1 - y*dimension_  - x];
+            //     break;
+            // case Rotation::R270:
+            //     cell = shapes_[rotation_chunk + dimension_ - 1 + x*dimension_ - y];
+            //     break;
+            // default:
+            //     break;
+            // }
 
+            // find the block that describes the shape of the current rotation
+            // eg, first dimension*dimension entries describes shape of rotation 0
+            int rotation_chunk = static_cast<int>(current_rotation_) * dimension_ * dimension_;
+            bool cell = shapes_[rotation_chunk + y*dimension_ + x];
             if (cell)
 				board_.DrawCell(board_position_ + Vec2<int>{x, y}, color_);
         }
@@ -52,4 +61,12 @@ void Tetromino::RotateCCW() {
     static constexpr Rotation counter_clockwise_rotations[] = 
         {Rotation::R270, Rotation::R0, Rotation::R90, Rotation::R180};
     current_rotation_ = counter_clockwise_rotations[static_cast<int>(current_rotation_)];
+}
+
+void Tetromino::MoveRight() {
+
+}
+
+void Tetromino::MoveLeft() {
+
 }
--- src/Tetromino.h
+++ src/Tetromino.h
@@ -17,7 +17,7 @@ public:
     };
 
 private:
-    const bool* shape_;
+    const bool* shapes_;
     const int dimension_;
     const Color color_;
     const Board& board_;
@@ -27,7 +27,7 @@ private:
     bool IsCollision();
 
 public:
-    Tetromino(const bool* shape, int dimension, Color color, const Board& board);
+    Tetromino(const bool* shapes, int dimension, Color color, const Board& board);
     void Draw() const;
     void RotateCW();
     void Rotate180();
@@ -38,113 +38,218 @@ public:
 
 class IMino : public Tetromino {
 private:
-    static constexpr bool shape_[] {0, 0, 0, 0,
-                                    1, 1, 1, 1,
-                                    0, 0, 0, 0,
-                                    0, 0, 0, 0};
     static constexpr int dimension_ = 4;
+    static constexpr bool shapes_[] 
+        {
+            0, 0, 0, 0,     // Rotation 0
+            1, 1, 1, 1,
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+        
+            0, 0, 1, 0,     // Rotation 90
+            0, 0, 1, 0,
+            0, 0, 1, 0,
+            0, 0, 1, 0,
+        
+            0, 0, 0, 0,     // Rotation 180
+            0, 0, 0, 0,
+            1, 1, 1, 1,
+            0, 0, 0, 0,
+        
+            0, 1, 0, 0,     // Rotation 270
+            0, 1, 0, 0,
+            0, 1, 0, 0,
+            0, 1, 0, 0
+        };
     static constexpr Color color_ = SKYBLUE;
 
 public:
     IMino(Board& board)
-        : Tetromino(shape_, dimension_, color_, board)
+        : Tetromino(shapes_, dimension_, color_, board)
     {
-        static_assert(sizeof(shape_) / sizeof(bool) == dimension_ * dimension_);
+        static_assert(sizeof(shapes_) / sizeof(bool) == 4 * dimension_ * dimension_);
     }
 };
 
 class OMino : public Tetromino {
 private:
-    static constexpr bool shape_[] {1, 1,
-                                    1, 1};
+    static constexpr bool shapes_[] 
+        {
+            1, 1,
+            1, 1,
+
+            1, 1,
+            1, 1,
+            
+            1, 1,
+            1, 1,
+            
+            1, 1,
+            1, 1
+        };
     static constexpr int dimension_ = 2;
     static constexpr Color color_ = YELLOW;
 
 public:
     OMino(Board& board)
-        : Tetromino(shape_, dimension_, color_, board)
+        : Tetromino(shapes_, dimension_, color_, board)
     {
-        static_assert(sizeof(shape_) / sizeof(bool) == dimension_ * dimension_);
+        static_assert(sizeof(shapes_) / sizeof(bool) == 4 * dimension_ * dimension_);
     }
 };
 
 class TMino : public Tetromino {
 private:
-    static constexpr bool shape_[] {0, 1, 0,
-                                    1, 1, 1,
-                                    0, 0, 0};
+    static constexpr bool shapes_[] 
+        {
+            0, 1, 0,
+            1, 1, 1,
+            0, 0, 0,
+
+            0, 1, 0,
+            0, 1, 1,
+            0, 1, 0,
+
+            0, 0, 0,
+            1, 1, 1,
+            0, 1, 0,
+
+            0, 1, 0,
+            1, 1, 0,
+            0, 1, 0,            
+        };
     static constexpr int dimension_ = 3;
     static constexpr Color color_ = MAGENTA;
 
 public:
     TMino(Board& board)
-        : Tetromino(shape_, dimension_, color_, board)
+        : Tetromino(shapes_, dimension_, color_, board)
     {
-        static_assert(sizeof(shape_) / sizeof(bool) == dimension_ * dimension_);
+        static_assert(sizeof(shapes_) / sizeof(bool) == 4 * dimension_ * dimension_);
     }
 };
 
 class JMino : public Tetromino {
 private:
-    static constexpr bool shape_[] {1, 0, 0,
-                                    1, 1, 1,
-                                    0, 0, 0};
+    static constexpr bool shapes_[] 
+        {
+            1, 0, 0,
+            1, 1, 1,
+            0, 0, 0,
+
+            0, 1, 1,
+            0, 1, 0,
+            0, 1, 0,
+
+            0, 0, 0,
+            1, 1, 1,
+            0, 0, 1,
+
+            0, 1, 0,
+            0, 1, 0,
+            1, 1, 0
+        };
     static constexpr int dimension_ = 3;
     static constexpr Color color_ = DARKBLUE;
 
 public:
     JMino(Board& board)
-        : Tetromino(shape_, dimension_, color_, board)
+        : Tetromino(shapes_, dimension_, color_, board)
     {
-        static_assert(sizeof(shape_) / sizeof(bool) == dimension_ * dimension_);
+        static_assert(sizeof(shapes_) / sizeof(bool) == 4 * dimension_ * dimension_);
     }
 };
 
 class LMino : public Tetromino {
 private:
-    static constexpr bool shape_[] {0, 0, 1,
-                                    1, 1, 1,
-                                    0, 0, 0};
+    static constexpr bool shapes_[] 
+        {
+            0, 0, 1,
+            1, 1, 1,
+            0, 0, 0,
+
+            0, 1, 0,
+            0, 1, 0,
+            0, 1, 1,
+
+            0, 0, 0, 
+            1, 1, 1, 
+            1, 0, 0, 
+
+            1, 1, 0, 
+            0, 1, 0, 
+            0, 1, 0
+        };
     static constexpr int dimension_ = 3;
     static constexpr Color color_ = ORANGE;
 
 public:
     LMino(Board& board)
-        : Tetromino(shape_, dimension_, color_, board)
+        : Tetromino(shapes_, dimension_, color_, board)
     {
-        static_assert(sizeof(shape_) / sizeof(bool) == dimension_ * dimension_);
+        static_assert(sizeof(shapes_) / sizeof(bool) == 4 * dimension_ * dimension_);
     }
 };
 
 class SMino : public Tetromino {
 private:
-    static constexpr bool shape_[] {0, 1, 1,
-                                    1, 1, 0,
-                                    0, 0, 0};
+    static constexpr bool shapes_[] 
+        {
+            0, 1, 1,
+            1, 1, 0,
+            0, 0, 0,
+
+            0, 1, 0, 
+            0, 1, 1,
+            0, 0, 1,
+
+            0, 0, 0,
+            0, 1, 1,
+            1, 1, 0,
+
+            1, 0, 0,
+            1, 1, 0, 
+            0, 1, 0 
+        };
     static constexpr int dimension_ = 3;
     static constexpr Color color_ = GREEN;
 
 public:
     SMino(Board& board)
-        : Tetromino(shape_, dimension_, color_, board)
+        : Tetromino(shapes_, dimension_, color_, board)
     {
-        static_assert(sizeof(shape_) / sizeof(bool) == dimension_ * dimension_);
+        static_assert(sizeof(shapes_) / sizeof(bool) == 4 * dimension_ * dimension_);
     }
 };
 
 class ZMino : public Tetromino {
 private:
-    static constexpr bool shape_[] {1, 1, 0,
-                                    0, 1, 1,
-                                    0, 0, 0};
+    static constexpr bool shapes_[] 
+        {
+            1, 1, 0,
+            0, 1, 1,
+            0, 0, 0,
+
+            0, 0, 1, 
+            0, 1, 1, 
+            0, 1, 0, 
+
+            0, 0, 0, 
+            1, 1, 0, 
+            0, 1, 1, 
+
+            0, 1, 0, 
+            1, 1, 0, 
+            1, 0, 0
+        };
     static constexpr int dimension_ = 3;
     static constexpr Color color_ = RED;
 
 public:
     ZMino(Board& board)
-        : Tetromino(shape_, dimension_, color_, board)
+        : Tetromino(shapes_, dimension_, color_, board)
     {
-        static_assert(sizeof(shape_) / sizeof(bool) == dimension_ * dimension_);
+        static_assert(sizeof(shapes_) / sizeof(bool) == 4 * dimension_ * dimension_);
     }
 };
 
