d91e0d8b4c5be913318b5960eb0c87239f779aeb
senwa105 <senwa105@gmail.com>
2023-01-06 21:17:39 -0500

    declared Randomizer class

---
+++ src/Randomizer.h
@@ -0,0 +1,39 @@
+#ifndef RANDOMIZER_HPP
+#define RANDOMIZER_HPP
+
+#include <array>
+
+#include "raylibcpp.h"
+#include "MinoType.hpp"
+
+class Randomizer {
+protected:
+    static constexpr std::array<mino_type::Mino, 7> minos 
+        {
+            mino_type::I,
+            mino_type::O,
+            mino_type::T,
+            mino_type::J,
+            mino_type::L,
+            mino_type::S,
+            mino_type::Z
+        };
+
+public:
+    virtual mino_type::Mino GetNextTetromino() = 0;
+};
+
+class BagRandomizer : public Randomizer {
+private:
+    bool used[minos.size()];
+
+public:
+    mino_type::Mino GetNextTetromino() override;
+};
+
+class CompleteRandomizer : public Randomizer {
+public:
+    mino_type::Mino GetNextTetromino() override;
+};
+
+#endif
