303e7a049ccaf749891adeba2e603b437aaa6b5f
senwa105 <senwa105@gmail.com>
2023-01-06 22:05:09 -0500

    implemented BagRandomizer::GetNextTetromino()

--- src/Randomizer.cpp
+++ src/Randomizer.cpp
@@ -1,9 +1,17 @@
 #include "Randomizer.h"
 
+#include <algorithm>
+
 #include "raylibcpp.h"
 
 mino_type::Mino BagRandomizer::GetNextTetromino() {
+    if (minos_left.size() == 0)
+        minos_left = {0, 1, 2, 3, 4, 5, 6};
 
+    int i = GetRandomValue(0, minos_left.size()-1);
+    int next_mino = minos_left[i];
+    minos_left.erase(minos_left.begin() + i);
+    return minos[next_mino];
 }
 
 mino_type::Mino CompleteRandomizer::GetNextTetromino() {
