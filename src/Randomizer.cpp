ea763efb5393602c7113b395ffcf5710001854e5
senwa105 <senwa105@gmail.com>
2023-01-14 23:24:33 -0500

    cleaned up BagRandomizer::GetNextTetromino()

--- src/Randomizer.cpp
+++ src/Randomizer.cpp
@@ -6,13 +6,21 @@
 #include "MinoType.hpp"
 
 const MinoType BagRandomizer::GetNextTetromino() {
-    if (minos_left.size() == 0)
-        minos_left = {0, 1, 2, 3, 4, 5, 6};
+    if (minos_left_.size() == 0)
+        minos_left_ = {
+            MinoType::I,
+            MinoType::O,
+            MinoType::T,
+            MinoType::J,
+            MinoType::L,
+            MinoType::S,
+            MinoType::Z
+        };
 
-    int i = GetRandomValue(0, minos_left.size()-1);
-    int next_mino = minos_left[i];
-    minos_left.erase(minos_left.begin() + i);
-    return static_cast<MinoType>(next_mino);
+    int i = GetRandomValue(0, minos_left_.size()-1);
+    MinoType next_mino = minos_left_[i];
+    minos_left_.erase(minos_left_.begin() + i);
+    return next_mino;
 }
 
 const MinoType CompleteRandomizer::GetNextTetromino() {
